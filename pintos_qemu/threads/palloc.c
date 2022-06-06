9003c80f91b57cbbf211a337920be399d67a081e
gimangi <khk211113@naver.com>
2022-06-07 06:03:50 +0900

    test

--- pintos_qemu/threads/palloc.c
+++ pintos_qemu/threads/palloc.c
@@ -85,8 +85,8 @@ palloc_get_multiple (enum palloc_flags flags, size_t page_cnt)
     return NULL;
 
   lock_acquire (&pool->lock);
-  //page_idx = bitmap_scan_and_flip (pool->used_map, 0, page_cnt, false); // TODO : modify
-  page_idx = bitmap_scan_buddy_and_flip (pool->used_map, page_cnt, false);
+  page_idx = bitmap_scan_and_flip (pool->used_map, 0, page_cnt, false); // TODO : modify
+  //page_idx = bitmap_scan_buddy_and_flip (pool->used_map, page_cnt, false);
 
   lock_release (&pool->lock);
 
