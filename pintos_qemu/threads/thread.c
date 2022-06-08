66630135e1bf46de15c839cec0f02200edd9cc9c
gimangi <khk211113@naver.com>
2022-06-08 23:45:53 +0900

    debug thread_current

--- pintos_qemu/threads/thread.c
+++ pintos_qemu/threads/thread.c
@@ -148,6 +148,7 @@ void
 thread_tick (void) 
 {
   struct thread *t = thread_current ();
+  printf("[debug] cur thread : %s", t->name);
 
   /* Update statistics. */
   if (t == idle_thread)
@@ -306,6 +307,7 @@ thread_sleep (int64_t tick)
 
   old_level = intr_disable ();
   cur = thread_current ();
+  printf("[debug] cur thread : %s", cur->name);
 
   ASSERT (cur != idle_thread);
 
@@ -402,6 +404,7 @@ void
 thread_yield (void) 
 {
   struct thread *cur = thread_current ();
+  printf("[debug] cur thread : %s", cur->name);
   enum intr_level old_level;
   
   ASSERT (!intr_context ());
